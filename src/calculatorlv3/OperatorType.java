e430ab1d7bcf7c4c73dad22fc9b20b49c9a920ee
aneunwook <dmsdnr0720@naver.com>
2025-04-22 16:38:23 +0900

    char 값을 기반으로 OperatorType으로 변환해주는 메서드 구현

--- src/calculatorlv3/OperatorType.java
+++ src/calculatorlv3/OperatorType.java
@@ -11,4 +11,18 @@ public enum OperatorType {
     OperatorType(String symbol) {
         this.symbol = symbol;
     }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public static OperatorType operatorChar (char symbol){
+        for(OperatorType op : OperatorType.values()){
+            if(op.getSymbol().charAt(0) ==  symbol){
+                return op;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산자입니다: " + symbol);
+
+    }
 }
